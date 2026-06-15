31207d6e1e1247ccf6d1d64e90c1ad8d8bd05b12
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-06-16 01:27:33 +0530

    Frontend connected to Render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://retail-sales-forecasting-platform.onrender.com"
 });
+
 
 export default API;
 
